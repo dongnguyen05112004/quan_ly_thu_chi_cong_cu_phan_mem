5d4a9e84507dc4147da1969f0f65a8c8ca1d3500
Duck <duc240804@gmail.com>
2025-07-11 11:56:35 +0700

    Duck

--- src/components/Khach_Hang/dang_nhap.vue
+++ src/components/Khach_Hang/dang_nhap.vue
@@ -8,20 +8,19 @@
                             <h3 class="fw-bold">Đăng Nhập</h3>
                         </div>
 
-                        <form >
                             <div class="mb-3">
                                 <label for="email" class="form-label">Email</label>
-                                <input  type="email" class="form-control" id="email"
+                                <input v-model="taikhoan.email" type="email" class="form-control" id="email"
                                     placeholder="Nhập email">
                             </div>
 
                             <div class="mb-4">
                                 <label for="password" class="form-label">Mật khẩu</label>
-                                <input  type="password" class="form-control" id="password"
+                                <input v-model="taikhoan.mat_khau" type="password" class="form-control" id="password"
                                     placeholder="Nhập mật khẩu">
                             </div>
 
-                            <button type="submit" class="btn btn-primary w-100 mb-3">
+                            <button v-on:click="Login()" class="btn btn-primary w-100 mb-3">
                                 Đăng Nhập
                             </button>
 
@@ -32,7 +31,6 @@
                                     Đăng ký tài khoản
                                 </router-link>
                             </div>
-                        </form>
                     </div>
                 </div>
             </div>
@@ -40,11 +38,38 @@
     </div>
 </template>
 <script >
+import axios from 'axios';
 export default {
-
+    data() {
+        return {
+            taikhoan: {
+                email : '',
+                mat_khau: '',
+            }
+        }
+    },
+    methods:{
+        Login(){
+            axios
+            .post("http://127.0.0.1:8000/api/khach-hang/dang-nhap", this.taikhoan)
+                .then((res) => {
+                    if (res.data.status) {
+                        this.$toast.success(res.data.message);
+                        localStorage.setItem('tai_khoan_login', res.data.token);
+                        this.$router.push('/home');
+                    } else {
+                        this.$toast.error(res.data.message);
+                    }
+                })
+                .catch((res) => {
+                    const list = Object.values(res.response.data.errors);
+                    list.forEach((v, i) => {
+                        this.$toast.error(v[0]);
+                    });
+                })
+        }
+    }
 }
 
 
 </script>
-<style>
-</style>
